4c86c770a5d5e57d7ef64cf03d725428db7c1197
ea2773804-tech <ea2773804@gmail.com>
2026-04-29 11:18:20 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
-import { getFirestore, collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-const firebaseConfig = {
-  // você vai colocar depois
-};
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyCv-84pYoBuUpQ8triEpboEhzxLWwverQA",
+    authDomain: "neoaki.firebaseapp.com",
+    projectId: "neoaki",
+    storageBucket: "neoaki.firebasestorage.app",
+    messagingSenderId: "421651948414",
+    appId: "1:421651948414:web:a1e45b92a55588ed44492f"
+  };
 
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-window.postar = async function () {
-  const texto = document.getElementById("postInput").value;
-
-  await addDoc(collection(db, "posts"), {
-    texto: texto,
-    data: new Date()
-  });
-
-  carregarPosts();
-};
-
-async function carregarPosts() {
-  const feed = document.getElementById("feed");
-  feed.innerHTML = "";
-
-  const querySnapshot = await getDocs(collection(db, "posts"));
-  querySnapshot.forEach((doc) => {
-    feed.innerHTML += `<p>${doc.data().texto}</p>`;
-  });
-}
-
-carregarPosts();
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
