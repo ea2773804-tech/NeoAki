3071d6fb846766e125ef3feb85654f4f81d4dcb4
ea2773804-tech <ea2773804@gmail.com>
2026-05-03 15:33:34 -0300

    Delete app.js

--- app.js
+++
@@ -1,5 +0,0 @@
-function changeVideo(src) {
-  const video = document.getElementById('main-video');
-  video.src = src; // Altera a origem do arquivo
-  video.play();    // Dá play automaticamente após trocar
-}
